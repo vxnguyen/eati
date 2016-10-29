e7ea82488af67791bffac30bf9375063c3198e64
Vinh Nguyen <vxnguyen01@gmail.com>
2016-10-29 19:10:56 -0400

    Add EATI theme files to git

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package EATI
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'eati' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'eati' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'eati' ), 'eati', '<a href="http://vinhnguyen.me" rel="designer">Vinh Nguyen</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
